5a003f683305da34b7277b6692508762c0fa6674
Anya <kannie327@gmail.com>
2024-11-29 12:41:56 +0200

    add runGame function to index.js

--- src/index.js
+++ src/index.js
@@ -20,4 +20,32 @@ const getUserAnswer = () => readlineSync.question('Your answer: ');
 //проверка ответа пользователя
 const checkAnswer = (userAnswer, expectedAnswer) => expectedAnswer === userAnswer;
 
-export { whatIsYourName, getNum, getUserAnswer, checkAnswer };
+const runGame = (description, getRoundData) => {
+    const maxRound = 3;
+    let correctAnswer = 0;
+
+    const name = whatIsYourName();
+
+    console.log(description);
+
+    while (correctAnswer < maxRound) {
+        const [question, expectedAnswer] = getRoundData();
+        console.log(`Question: ${question}`);
+
+        const userAnswer = getUserAnswer();
+        const isCorrect = checkAnswer(userAnswer, expectedAnswer);
+
+        if (isCorrect) {
+            correctAnswer += 1;
+            console.log('Correct!');
+        } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${name}!`);
+};
+
+export { getNum, runGame };
