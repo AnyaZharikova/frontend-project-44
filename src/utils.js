8ec40e170bfe5612dd8185650c15e6749e39eca8
Anya <kannie327@gmail.com>
2024-12-04 20:59:14 +0200

    Fix linter issues

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,14 @@
 let seed = Date.now();
 
 const getNum = (min = 1, max = 50) => {
-  
-  //LCG parametrs from RANDU
+
+  // LCG parametrs from RANDU
   const modulus = 2 ** 31;
   const multiplier = 1103515245;
   const increment = 12345;
 
   seed = (multiplier * seed + increment) % modulus;
-  
+
   const randomNum = Math.floor((seed / modulus) * (max - min + 1) + min);
 
   return randomNum;
@@ -20,10 +20,8 @@ const getOperator = (operators) => {
   const multiplier = 1103515245;
   const increment = 12345;
 
-  let seed = Date.now();
-
   seed = (multiplier * seed + increment) % modulus;
-  
+
   const randomIndex = seed % operators.length;
 
   return operators[randomIndex];
